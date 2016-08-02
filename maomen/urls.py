71cb47a86486536a024d0da30c094bbdb0eee48e
dongshaohui <405989455@qq.com>
2016-08-02 16:04:14 +0800

    修改bug

--- maomen/urls.py
+++ maomen/urls.py
@@ -43,7 +43,7 @@ urlpatterns = patterns('',
 
 	url(r'^wallet/top_up_items','broadcast.views.top_up_items'),
    	url(r'^wallet/confirm_receipt','broadcast.views.confirm_receipt'), # ios验证凭证
-   	url(r'^wallet/check_receipt','broadcast.views.check_receipt')
+   	url(r'^wallet/check_receipt','broadcast.views.check_receipt'),
     # 客户端轮训
     url(r'^channel/client/round_check','broadcast.views.round_check'),
     # sign
